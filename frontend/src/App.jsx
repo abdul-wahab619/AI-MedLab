233c7747480de708c338e060ed20fd9473a07593
Abdul Wahab <abdulwahab@gmail.com>
2024-02-15 12:40:50 +0500

    app.css file loc changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 function App() {
   return (
     <>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import'./App.css'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
