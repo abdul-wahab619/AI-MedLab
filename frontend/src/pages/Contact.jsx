d665a5fea6388f82e2c0e9c76abb054ce6b2fb0e
Abdul Wahab <abdulwahab@gmail.com>
2024-02-16 21:02:11 +0500

    Contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,56 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section>
+      <div className="px-4 mx-auto max-w-screen-md">
+        <h2 className="heading text-center">Contact Us</h2>
+        <p className="mb-8 lg:mb-18 font-light text-center text__para">
+          Got a technical issue? want to send feedback about a beta feature? Let
+          us know.
+        </p>
+        <form action="#" className="space-y-8">
+          <div>
+            <label htmlFor="email" className="form__label">
+              Your Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="example@gmail.com "
+              className="form__input mt-1"
+            />
+          </div>
+          <div>
+            <label htmlFor="subject" className="form__label">
+              Subject
+            </label>
+            <input
+              type="text"
+              id="subject"
+              placeholder="Let us know about the issue... "
+              className="form__input mt-1"
+            />
+          </div>
+          <div>
+            <label htmlFor="message" className="form__label">
+              Message
+            </label>
+            <textarea
+              rows={6}
+              type="text"
+              id="message"
+              placeholder="write the details message here... "
+              className="form__input mt-1"
+            />
+          </div>
+          <button type="submit" className="btn rounded sm:w-fit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
