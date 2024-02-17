255d63fba951a1e298231afdef370e5dae68bdfa
Abdul Wahab <abdulwahab@gmail.com>
2024-02-17 23:21:48 +0500

    Login page integrate with backend

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,52 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+
+const initialState = {
+  user: null,
+  role: null,
+  token: null,
+};
+
+export const authContext = createContext(initialState);
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN_START":
+      return {
+        user: null,
+        role: null,
+        token: null,
+      };
+    case "LOGIN_SUCCESS":
+      return {
+        user: action.payload.user,
+        token: action.payload.token,
+        role: action.payload.role,
+      };
+    case "LOGOUT":
+      return {
+        user: null,
+        role: null,
+        token: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  return (
+    <authContext.Provider
+      value={{
+        user: state.user,
+        token: state.token,
+        role: state.role,
+        dispatch,
+      }}
+    >
+      {children}
+    </authContext.Provider>
+  );
+};
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,62 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../../config";
+import { toast } from "react-toastify";
+import HashLoader from "react-spinners/HashLoader";
+import { authContext } from "../context/AuthContext.jsx";
 
 const Login = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { dispatch } = useContext(authContext);
+
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
+
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
+
+  const submitHandler = async (event) => {
+    // console.log(formData);
+    event.preventDefault();
+    setLoading(true);
+
+    try {
+      const res = await fetch(`${BASE_URL}/auth/login`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const result = await res.json();
+      if (!res.ok) {
+        throw new Error(result.message);
+      }
+
+      dispatch({
+        type: "LOGIN_SUCCESS",
+        payload: {
+          user: result.data,
+          token: result.token,
+          role: result.role,
+        },
+      });
+
+      console.log(result, " login data");
+
+      setLoading(false);
+      toast.success(result.message);
+      navigate("/home");
+    } catch (err) {
+      toast.error(err.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <section className="px-5 lg:px-0">
       <div className="w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10">
@@ -16,7 +64,7 @@ const Login = () => {
           Hello
           <span className="text-primaryColor"> Welcome </span> Back 👋
         </h3>
-        <form action="" className="py-4 md:py-0">
+        <form action="" className="py-4 md:py-0" onSubmit={submitHandler}>
           <div className="mb-5">
             <input
               type="email"
