900fa352b5675f7bc2dfde0585832b467cb42ccc
Nafeesa-shehzadi <86777809+Nafeesa-shehzadi@users.noreply.github.com>
2024-06-01 18:16:58 +0500

    added  new field

--- frontend/src/Dashboard/doctor-account/Appointments.jsx
+++ frontend/src/Dashboard/doctor-account/Appointments.jsx
@@ -21,6 +21,9 @@ const Appointments = ({ appointments }) => {
           <th scope="col" className="px-6 py-6">
             Booked on
           </th>
+          <th scope="col" className="px-6 py-6">
+            Test name
+          </th>
         </tr>
       </thead>
 
@@ -31,38 +34,17 @@ const Appointments = ({ appointments }) => {
               scope="row"
               className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap"
             >
-              <img
-                src={item.user.photo}
-                className="w-10 h-10 rounded-full"
-                alt=""
-              />
               <div className="pl-3">
-                <div className="text-base font-semibold">{item.user.name}</div>
-                <div className="text-normal text-gray-500">
-                  {item.user.email}
+                <div className="text-base font-semibold">
+                  {item.patientName}
                 </div>
               </div>
             </th>
-
-            <td className="px-6 py-4">{item.user.gender}</td>
-            <td className="px-6 py-4">
-              {item.isPaid && (
-                <div className="flex items-center">
-                  <div className="h-2 5 w-2 5 rounded-full bg-green-500 mr-2"></div>
-                  Paid
-                </div>
-              )}
-
-              {!item.isPaid && (
-                <div className="flex items-center">
-                  <div className="h-2 5 w-2 5 rounded-full bg-red-500 mr-2"></div>
-                  UnPaid
-                </div>
-              )}
-            </td>
-
-            <td className="px-6 py-4">{item.ticketPrice}</td>
-            <td className="px-6 py-4">{formateDate(item.createdAt)}</td>
+            <td className="px-6 py-4">{item.patientGender}</td>{" "}
+            <td className="px-6 py-4">{item.payment}</td>
+            <td className="px-6 py-4">{item.price}</td>
+            <td className="px-6 py-4">{formateDate(item.bookedOn)}</td>
+            <td className="px-6 py-4">{item.testName}</td>
           </tr>
         ))}
       </tbody>
